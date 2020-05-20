120dc628f57be08ea4190de39fff0bd367ff7592
Flavian <flavian.mohanraj@gmail.com>
2020-05-19 20:59:40 -0400

    using props to dynamically style buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import "./App.css";
 import Person from "./Person/Person";
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -45,14 +60,14 @@ class App extends Component {
   render() {
     const style = {
       backgroundColor: "green",
-      color: 'white',
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
       cursor: "pointer",
-      ':hover':{
-        backgroundColor: 'lightgreen',
-        color: 'black'
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black"
       }
     };
 
@@ -74,31 +89,30 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
+      // style.backgroundColor = "red";
+      // style[":hover"] = {
+      //   backgroundColor: "salmon",
+      //   color: "black"
+      // };
     }
 
     let classes = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      classes.push("red");
     }
-    if (this.state.persons.length <=1) {
-      classes.push('bold');
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working</p>
-        <button style={style} onClick={this.togglePersonHandler}>
+        <p className={classes.join(" ")}>This is really working</p>
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonHandler}>
           Toggle Persons
-        </button>
+        </StyledButton>
         {persons}
       </div>
-
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now') )
   }
