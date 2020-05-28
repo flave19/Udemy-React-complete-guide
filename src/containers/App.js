857f646e90863cf1f5f9167b23f55ee03fc8d434
Flavian <flavian.mohanraj@gmail.com>
2020-05-28 09:09:30 -0400

    a better project structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import classes from "./App.css";
-import Person from "../components/Person/Person";
+import Person from "../components/Persons/Person/Person";
 
 
 
