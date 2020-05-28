a3302858565caedc961613080e2d43fcf4304d2b
Flavian <flavian.mohanraj@gmail.com>
2020-05-28 14:05:28 -0400

    Component creation lifecycle

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,13 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit"
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor')
+
+  }
+
+
   state = {
     persons: [
       { id: "1asda", name: "Flav", age: 26 },
@@ -14,6 +21,19 @@ class App extends Component {
     otherState:'some other value',
     showPersons: false
   };
+
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] getDerivedStateFromProps', props)
+    return state
+  }
+
+  componentWillMount(){
+    console.log('[App.js] componentWillMount')
+  }
+
+  componentDidMount(){
+    console.log('[App.js] componentDidMount')
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -45,6 +65,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('[App.js] render')
     let persons = null;
 
     if (this.state.showPersons) {
