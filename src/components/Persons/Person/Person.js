ddb5ae93a99d8c61bde6128371050a77f52095d2
Flavian <flavian.mohanraj@gmail.com>
2020-06-25 16:36:14 -0400

    using Aux to display adjacent rendering vs displayinf props children

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
-
+import Aux from '../../../hoc/Aux';
 import classes from "./Person.css";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
-    return [
+    return (
+      <Aux>
         <p key="i1" onClick={this.props.click}>
           Im {this.props.name} and I am {this.props.age} years old!
-        </p>,
-        <p key="i2" >{this.props.children}</p>,
+        </p>
+        <p key="i2" >{this.props.children}</p>
         <input
           key="i3"
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-    ];
+      </Aux>
+    );
   }
 }
 
